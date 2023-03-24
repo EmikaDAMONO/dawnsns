80ccbc2d941e05c8e944ff828767b02c15909a49
Emika <bunya08boss@gmail.com>
2023-03-24 23:11:45 +0900

    no message

--- database/migrations/2019_12_24_090209_create_posts_table.php
+++ database/migrations/2019_12_24_090209_create_posts_table.php
@@ -15,13 +15,14 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id')->autoIncrement();
-            $table->integer('user_id');
+            $table->integer('user_id');//外部キー１対多
             $table->string('post',400);
             $table->timestamp('created_at')
             ->useCurrent();
             $table->timestamp('modified_at')
             ->useCurrent()
             ->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('user');
         });
     }
 
